c89da05c4f309a0de2b803554daf97a070eae3df
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-21 17:42:55 -0500

    Use Quote component inside app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <Calculator />
+      <Quote />
+    </>
   );
 }
 
