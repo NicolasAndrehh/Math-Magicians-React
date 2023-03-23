d0b9f9f499ae8050cc8c357aad9a35ba486f07b4
Nicolas Olaya <111377816+NicolasAndrehh@users.noreply.github.com>
2023-03-23 16:00:18 -0500

    Add title to calculator component

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -18,17 +18,20 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator">
+    <section className="calculator-section">
+      <h2>Let&apos;s do some Math!</h2>
+      <div className="calculator">
 
-      <div className="result">
-        <p>{ resultState.total }</p>
-        <p>{ resultState.operation }</p>
-        <p>{ resultState.next }</p>
+        <div className="result">
+          <p>{ resultState.total }</p>
+          <p>{ resultState.operation }</p>
+          <p>{ resultState.next }</p>
+        </div>
+        <div className="buttons">
+          { createButtons(changeResult) }
+        </div>
       </div>
-      <div className="buttons">
-        { createButtons(changeResult) }
-      </div>
-    </div>
+    </section>
   );
 };
 
